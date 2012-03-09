0f8ed22a30b9b1feddcbe3c7890c1272875a0aae
Joseph Daigle <joseph@cridion.com>
2012-03-09 12:19:55 -0500

    adding message mapper type and tests

---
+++ CWServiceBus.UnitTests/Reflection/When_mapping_interfaces.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using CWServiceBus.Reflection;
+
+namespace CWServiceBus.Reflection
+{
+    [TestFixture]
+    public class When_mapping_interfaces
+    {
+        IMessageMapper mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mapper = new MessageMapper();
+        }
+
+        [Test]
+        public void Interfaces_with_only_properties_should_be_mapped()
+        {
+            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+
+            Assert.NotNull(mapper.GetMappedTypeFor(typeof(InterfaceWithProperties)));
+        }
+
+        [Test]
+        public void Interface_should_be_created()
+        {
+            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+
+            var result = mapper.CreateInstance<InterfaceWithProperties>(null);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Interfaces_with_methods_should_be_ignored()
+        {
+            mapper.Initialize(new[] {typeof(InterfaceWithMethods)});
+
+            Assert.Null(mapper.GetMappedTypeFor(typeof(InterfaceWithMethods)));
+        }
+
+
+        [Test]
+        public void Generated_type_should_preserve_namespace_to_make_it_easier_for_users_to_define_custom_conventions()
+        {
+            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+
+            Assert.AreEqual(typeof(InterfaceWithProperties).Namespace, mapper.CreateInstance(typeof(InterfaceWithProperties)).GetType().Namespace);
+        }
+
+
+        private bool PropertyContainsAttribute(string propertyName, Type attributeType, object obj)
+        {
+            return obj.GetType().GetProperty(propertyName).GetCustomAttributes(attributeType,true).Length > 0;
+        }
+    }
+
+    public interface InterfaceWithProperties : IMessage
+    {
+        string SomeProperty { get; set; }
+    }
+
+    public interface InterfaceWithMethods
+    {
+        string SomeProperty { get; set; }
+        void MethodOnInterface();
+    }
+}
