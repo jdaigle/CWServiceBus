0300b8b7818680bb89a65634b8199a32b6a5e173
Joseph Daigle <joseph@cridion.com>
2012-05-01 09:01:45 -0400

    changing name of extension method

--- CWServiceBus.Interfaces/MessageBusExtensionsMethods.cs
+++ CWServiceBus.Interfaces/MessageBusExtensionsMethods.cs
@@ -1,16 +1,22 @@
-﻿namespace CWServiceBus {
-    public static class MessageBusExtensionsMethods {
-        public static void CopyHeaderFromRequest(this IMessageBus messageBus) {
-            foreach (var item in messageBus.CurrentMessageContext.Headers) {
+﻿namespace CWServiceBus
+{
+    public static class MessageBusExtensionsMethods
+    {
+        public static void CopyHeadersFromRequest(this IMessageBus messageBus)
+        {
+            foreach (var item in messageBus.CurrentMessageContext.Headers)
+            {
                 messageBus.OutgoingHeaders[item.Key] = item.Value;
             }
         }
 
-        public static string GetHeader(this IMessageBus messageBus, string key) {
+        public static string GetHeader(this IMessageBus messageBus, string key)
+        {
             return messageBus.CurrentMessageContext.Headers.ContainsKey(key) ? messageBus.CurrentMessageContext.Headers[key] : string.Empty;
         }
 
-        public static string SetHeader(this IMessageBus messageBus, string key, string value) {
+        public static string SetHeader(this IMessageBus messageBus, string key, string value)
+        {
             return messageBus.OutgoingHeaders[key] = value;
         }
     }
