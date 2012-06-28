c22b8a5b4dd63f88f6975bae43aa331b7bb32072
Joseph Daigle <joseph@cridion.com>
2012-06-28 11:38:00 -0400

    created a helper library to work with out service broker databases

---
+++ CWServiceBus.ServiceBroker.Utils/PoisonMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace CWServiceBus.ServiceBroker.Utils
+{
+    public class PoisonMessage
+    {
+        public PoisonMessageInfo Info { get; set; }
+        public byte[] UTF8EncodedMessage { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+}
